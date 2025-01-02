4311e3f6f08d6334fffe56f953298b5d46f5c9b7
Fahmi Akbar Wildana <sensorfied@gmail.com>
2025-01-02 16:07:27 +0700

    Add GLTF exporter stories

---
+++ stories/Exporter/GLTF.stories.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react"
+import { CadViewer, useExportGltfUrl, useSaveGltfAs } from "../../src/index.tsx"
+import { Circuit } from "@tscircuit/core"
+
+export default { title: "GLTF exporter" }
+
+export const DownloadLink = () => {
+  const [ref, url] = useExportGltfUrl()
+
+  return <>
+    <a download="pcb.gltf" href={url}>download</a>
+
+    <CadViewer ref={ref}>
+      <board width="10mm" height="10mm">
+        <capacitor
+          capacitance="1000pF"
+          footprint="0402"
+          name="C1"
+          pcbRotation={90}
+        />
+      </board>
+    </CadViewer>
+  </>
+}
+
+export const SaveButton = () => {
+  const [ref, saveAs] = useSaveGltfAs()
+
+  return <>
+    <button onClick={() => saveAs("pcb.gltf")}>save</button>
+
+    <CadViewer ref={ref}>
+      <board width="10mm" height="40mm">
+        <chip name="U1" footprint="tssop16" />
+      </board>
+    </CadViewer>
+  </>
+}
