1ec710d6e10b3963573d2b0b9f4a4714bb56f46b
Your Name <you@example.com>
2025-09-09 19:02:37 +0200

    story test for gltf

--- stories/Models.stories.tsx
+++ stories/Models.stories.tsx
@@ -1,16 +1,38 @@
 import { CadViewer } from "src/CadViewer"
 import { Circuit } from "@tscircuit/core"
 import myGlb from "./assets/myGlb.glb?url"
+import myGltfUrl from "./assets/myGltf.gltf?url"
 import myObjUrl from "./assets/myObj.obj?url"
 
 export const GltfModel = () => (
   <CadViewer>
     <board width="5mm" height="5mm">
       <chip
+        name="Gltf"
+        footprint={
+          <footprint>
+            <hole diameter="0.9mm" pcbX={0} pcbY={0} />
+          </footprint>
+        }
+        cadModel={{
+          gltfUrl: myGltfUrl,
+          rotationOffset: { x: 90, y: 0, z: 0 },
+          positionOffset: { x: 0, y: 0, z: 0.6 },
+        }}
+      />
+    </board>
+  </CadViewer>
+)
+GltfModel.storyName = "GLTF Model"
+
+export const GlbModel = () => (
+  <CadViewer>
+    <board width="5mm" height="5mm">
+      <chip
         name="Glb"
         footprint={
           <footprint>
-            <hole diameter="0.8mm" pcbX={0} pcbY={0} />
+            <hole diameter="0.9mm" pcbX={0} pcbY={0} />
           </footprint>
         }
         cadModel={{
@@ -22,7 +44,7 @@ export const GltfModel = () => (
     </board>
   </CadViewer>
 )
-GltfModel.storyName = "GLTF Model"
+GlbModel.storyName = "GLB Model"
 
 const createObjCircuit = () => {
   const circuit = new Circuit()
@@ -40,7 +62,7 @@ const createObjCircuit = () => {
         }}
         footprint={
           <footprint>
-            <hole diameter="0.8mm" pcbX={0} pcbY={0} />
+            <hole diameter="0.9mm" pcbX={0} pcbY={0} />
           </footprint>
         }
       />
