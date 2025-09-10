6321f46f6407f668b1e6c20b0b13ef72ebdeac4e
seveibar <seveibar@gmail.com>
2025-09-09 20:04:57 -0700

    add proper async rendering

--- stories/KicadResistor.stories.tsx
+++ stories/KicadResistor.stories.tsx
@@ -1,26 +1,39 @@
-import { CadViewer } from "src/CadViewer"
-import { Circuit } from "@tscircuit/core"
+import { CadViewer } from "src/CadViewer";
+import { Circuit } from "@tscircuit/core";
+import { useEffect, useState } from "react";
 
-const createCircuit = () => {
-  const circuit = new Circuit()
-  circuit.add(
-    <board width="10mm" height="10mm">
-      <resistor
-        resistance="1k"
-        footprint="kicad:Resistor_SMD/R_0402_1005Metric"
-        name="R1"
-      />
-    </board>
-  )
-  return circuit.getCircuitJson()
-}
+const createCircuit = async () => {
+	const circuit = new Circuit();
+	circuit.add(
+		<board width="5mm" height="5mm">
+			<resistor
+				resistance="1k"
+				footprint="kicad:Resistor_SMD/R_0402_1005Metric"
+				name="R1"
+			/>
+		</board>,
+	);
+	await circuit.renderUntilSettled();
+	return circuit.getCircuitJson();
+};
 
 export const KicadResistor = () => {
-  const circuitJson = createCircuit()
-  return <CadViewer circuitJson={circuitJson as any} />
-}
+	const [circuitJson, setCircuitJson] = useState<any[] | null>(null);
+	useEffect(() => {
+		const renderCircuit = async () => {
+			const json = await createCircuit();
+			console.log(json.filter((item) => item.type === "cad_component"));
+			setCircuitJson(json);
+		};
+		renderCircuit();
+	}, []);
+	if (!circuitJson) {
+		return null;
+	}
+	return <CadViewer circuitJson={circuitJson as any} />;
+};
 
 export default {
-  title: "KicadResistor",
-  component: KicadResistor,
-}
+	title: "KicadResistor",
+	component: KicadResistor,
+};
