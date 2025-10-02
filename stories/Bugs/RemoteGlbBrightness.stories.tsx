38eb1f5650876b64311cf58e83500879cfab659f
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2025-10-02 08:40:00 +0200

    Add focused GLB brightness regression test

--- stories/Bugs/RemoteGlbBrightness.stories.tsx
+++ stories/Bugs/RemoteGlbBrightness.stories.tsx
@@ -1,48 +1,24 @@
-import type { AnyCircuitElement } from "circuit-json"
 import { CadViewer } from "src/CadViewer"
-
-const remoteGlbCircuit: AnyCircuitElement[] = [
-  {
-    type: "pcb_board",
-    pcb_board_id: "pcb_board_remote_glb",
-    center: { x: 0, y: 0 },
-    thickness: 1.6,
-    num_layers: 2,
-    material: "fr4",
-    width: 10,
-    height: 10,
-  },
-  {
-    type: "pcb_component",
-    pcb_component_id: "pcb_component_remote_glb",
-    source_component_id: "source_component_remote_glb",
-    center: { x: 0, y: 0 },
-    width: 1,
-    height: 1,
-    layer: "top",
-    rotation: 0,
-  },
-  {
-    type: "source_component",
-    source_component_id: "source_component_remote_glb",
-    name: "Remote GLB Chip",
-    ftype: "simple_resistor",
-    resistance: 1000,
-  },
-  {
-    type: "cad_component",
-    cad_component_id: "cad_component_remote_glb",
-    source_component_id: "source_component_remote_glb",
-    pcb_component_id: "pcb_component_remote_glb",
-    position: { x: 0, y: 0, z: 0.6 },
-    rotation: { x: 90, y: 0, z: 0 },
-    model_glb_url: "https://modelcdn.tscircuit.com/jscad_models/soic6.glb",
-  },
-]
 
 export const RemoteSoic6Glb = () => (
   <div style={{ width: 640, height: 480 }}>
-    <CadViewer circuitJson={remoteGlbCircuit} />
+    <CadViewer>
+      <board width="10mm" height="10mm">
+        <chip
+          name="Remote SOIC-6"
+          cadModel={{
+            glbUrl: "https://modelcdn.tscircuit.com/jscad_models/soic6.glb",
+            rotationOffset: { x: 90, y: 0, z: 0 },
+            positionOffset: { x: 0, y: 0, z: 0.6 },
+          }}
+          footprint={
+            <footprint>
+              <hole diameter="0.9mm" pcbX={0} pcbY={0} />
+            </footprint>
+          }
+        />
+      </board>
+    </CadViewer>
   </div>
 )
 
