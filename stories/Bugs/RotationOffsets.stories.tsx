84b5da46a47ddfa66bba7b6ce03f738fbabf4db6
techmannih <manishchaudharyttt@gmail.com>
2026-01-30 21:34:50 +0530

    f

--- stories/Bugs/RotationOffsets.stories.tsx
+++ stories/Bugs/RotationOffsets.stories.tsx
@@ -327,7 +327,6 @@ const circuitJson: AnyCircuitElement[] = [
     model_obj_url:
       "https://modelcdn.tscircuit.com/easyeda_models/download?uuid=c7acac53bcbc44d68fbab8f60a747688&pn=C17414",
     anchor_alignment: "center",
-
   },
   {
     type: "source_trace",
