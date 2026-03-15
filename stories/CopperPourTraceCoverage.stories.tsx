e13ffa9bf11d69814ffd2acc5923a579d1bd6ec7
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2026-03-15 15:46:46 +0200

    update circuit-to-canvas

---
+++ stories/CopperPourTraceCoverage.stories.tsx
@@ -0,0 +1,17 @@
+import { CadViewer } from "src/CadViewer"
+import differentNet from "./assets/copper-pour-different-net.json"
+import sameNet from "./assets/copper-pour-same-net.json"
+import sameNetFullyCovered from "./assets/copper-pour-same-net-trace-fully-covered.json"
+
+export const DifferentNet = () => <CadViewer circuitJson={differentNet as any} />
+
+export const SameNet = () => <CadViewer circuitJson={sameNet as any} />
+
+export const SameNetTraceFullyCovered = () => (
+  <CadViewer circuitJson={sameNetFullyCovered as any} />
+)
+
+export default {
+  title: "Copper Pour/Trace Coverage",
+  component: DifferentNet,
+}
