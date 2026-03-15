b88946c46633c74de42b7345642c903e72021bad
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2026-03-15 15:47:29 +0200

    format

--- stories/CopperPourTraceCoverage.stories.tsx
+++ stories/CopperPourTraceCoverage.stories.tsx
@@ -3,7 +3,9 @@ import differentNet from "./assets/copper-pour-different-net.json"
 import sameNet from "./assets/copper-pour-same-net.json"
 import sameNetFullyCovered from "./assets/copper-pour-same-net-trace-fully-covered.json"
 
-export const DifferentNet = () => <CadViewer circuitJson={differentNet as any} />
+export const DifferentNet = () => (
+  <CadViewer circuitJson={differentNet as any} />
+)
 
 export const SameNet = () => <CadViewer circuitJson={sameNet as any} />
 
