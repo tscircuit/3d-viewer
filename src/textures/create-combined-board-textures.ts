ffa26e60915c872a57ca57ecec6b5658bf34e6e6
Anas sarkiz <72264380+AnasSarkiz@users.noreply.github.com>
2026-03-17 02:37:27 +0200

    Refactor copper pour rendering pipeline to use CircuitToCanvasDrawer and fix missing/transparent board textures (#738)

--- src/textures/create-combined-board-textures.ts
+++ src/textures/create-combined-board-textures.ts
@@ -7,6 +7,7 @@ import { createPadTextureForLayer } from "../utils/pad-texture"
 import { createPanelOutlineTextureForLayer } from "../utils/panel-outline-texture"
 import { createTraceTextureForLayer } from "../utils/trace-texture"
 import { createCopperTextTextureForLayer } from "./create-copper-text-texture-for-layer"
+import { createCopperPourTextureForLayer } from "./create-copper-pour-texture-for-layer"
 import { createFabricationNoteTextureForLayer } from "./create-fabrication-note-texture-for-layer"
 import { createPcbNoteTextureForLayer } from "./create-pcb-note-texture-for-layer"
 import { createSilkscreenTextureForLayer } from "./create-silkscreen-texture-for-layer"
@@ -115,6 +116,16 @@ export function createCombinedBoardTextures({
         })
       : null
 
+    const copperPourTexture = showCopper
+      ? createCopperPourTextureForLayer({
+          layer,
+          circuitJson,
+          boardData,
+          traceTextureResolution,
+          copperColor,
+        })
+      : null
+
     const copperTextTexture = showCopper
       ? createCopperTextTextureForLayer({
           layer,
@@ -184,6 +195,7 @@ export function createCombinedBoardTextures({
 
     return createCombinedTexture({
       textures: [
+        copperPourTexture,
         traceTexture,
         padTexture,
         throughHoleTexture,
