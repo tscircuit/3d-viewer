16a013cfef8bb589cb5630de1e505ae93d954237
Rushabh Patil <mail@rushabh.dev>
2026-05-22 23:48:19 +0530

    formaT

--- stories/Pinheader.stories.tsx
+++ stories/Pinheader.stories.tsx
@@ -4,7 +4,7 @@ export const Pinheader = () => {
   return (
     <CadViewer>
       <board width={20} height={10}>
-        <pinheader gender="male" pinCount={2} name="P1" pcbX={-4}/>
+        <pinheader gender="male" pinCount={2} name="P1" pcbX={-4} />
         <pinheader gender="female" pinCount={2} name="P2" pcbX={4} />
       </board>
     </CadViewer>
