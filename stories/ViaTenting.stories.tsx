cd7c769fa2319db6f38005a000dd3a1694c2f1dd
techmannih <125847751+techmannih@users.noreply.github.com>
2026-09-22 10:43:07 +0530

    Add a story for silkscreen and pad overlap with tented vias

--- stories/ViaTenting.stories.tsx
+++ stories/ViaTenting.stories.tsx
@@ -1,9 +1,16 @@
 import { CadViewer } from "src/CadViewer"
 import viaTentingCircuit from "./assets/via-tenting.json"
+import viaTentingOverlapCircuit from "./assets/via-tenting-overlap.json"
 
 export const BoardDefaultsAndOverrides = () => (
   <div style={{ width: "100vw", height: "100vh" }}>
     <CadViewer circuitJson={viaTentingCircuit} />
+  </div>
+)
+
+export const SilkscreenAndPadOverlap = () => (
+  <div style={{ width: "100vw", height: "100vh" }}>
+    <CadViewer circuitJson={viaTentingOverlapCircuit} />
   </div>
 )
 
